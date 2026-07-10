e51b6d2df97487626f1bb008dd9fc2d4178b9438
joan <joan@email.com>
2026-07-10 04:42:01 +0100

    Remove notes.js file containing Mongoose operation timeout error logs

--- notes.js
+++
@@ -1,5 +0,0 @@
-2026-07-10 00:58:57.560 [error] [ErrorHandler] MongooseError: Operation `users.findOne()` buffering timed out after 10000ms
-    at Timeout.<anonymous> (/var/task/node_modules/mongoose/lib/drivers/node-mongodb-native/collection.js:187:23)
-    at listOnTimeout (node:internal/timers:605:17)
-    at process.processTimers (node:internal/timers:541:7)
-
